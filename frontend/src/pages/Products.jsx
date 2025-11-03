63471a7358ae1d2ba2e1a3539328634f8d0091a4
eyoasmulat <desalegnmulat5@gmail.com>
2025-11-03 08:19:44 -1100

    add some code in my app

---
+++ frontend/src/pages/Products.jsx
@@ -0,0 +1,15 @@
+﻿import ProductList from "../components/ProductList";
+import ProductForm from "../components/ProductForm";
+import { useState } from "react";
+
+export default function Products() {
+    const [refresh, setRefresh] = useState(false);
+
+    return (
+        <div>
+            <h2>Products</h2>
+            <ProductForm onAdded={() => setRefresh(r => !r)} />
+            <ProductList key={String(refresh)} />
+        </div>
+    );
+}
